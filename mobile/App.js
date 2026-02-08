587613e13edd4503c7416d600a91e1c1f26ab1da
Joshua Brendan Connelly <jconn020@odu.edu>
2026-02-08 13:36:48 -0500

    Recreate mobile app using Bun instead of Node.js

---
+++ mobile/App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
