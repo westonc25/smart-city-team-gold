93e26087bbfaa7833fcd75cc14ea064ec6357058
Joshua Brendan Connelly <jconn020@odu.edu>
2026-02-16 01:39:15 -0500

    Configured the Environment with both MapBox keys, setting up MapBox in react native and applying the base MapView Component. Basic controls already built in, added auto-tracking to current location.

--- mobile/app/(tabs)/map.tsx
+++ mobile/app/(tabs)/map.tsx
@@ -1,17 +1,64 @@
 import { StyleSheet } from 'react-native';
-import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { useRef, useEffect, useState } from 'react';
+import Mapbox from '@rnmapbox/maps';
+import * as Location from 'expo-location';
+
+Mapbox.setAccessToken('MAPBOX_TOKEN');
 
 export default function MapScreen() {
+  const cameraRef = useRef<Mapbox.Camera>(null);
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(null);
+
+  //Retrieves location
+  useEffect(() => 
+  {
+    const getLocation = async () => 
+    {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status === 'granted') 
+      {
+        const location = await Location.getCurrentPositionAsync({});
+        const { longitude, latitude } = location.coords;
+        setUserLocation([longitude, latitude]);
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  //Moves to location upon update
+  useEffect(() => 
+  {
+    if (userLocation && cameraRef.current) 
+    {
+      cameraRef.current.setCamera(
+      {
+        centerCoordinate: userLocation,
+        zoomLevel: 14,
+        animationDuration: 1000,
+      });
+    }
+  }, [userLocation]);
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">Map</ThemedText>
-      <ThemedText>Map view placeholder</ThemedText>
+    <ThemedView style={{ flex: 1 }}>
+      <Mapbox.MapView style={{ flex: 1 }}>
+        <Mapbox.Camera
+          ref={cameraRef}
+          followUserLocation={true}
+          followZoomLevel={14}
+        />
+
+        <Mapbox.UserLocation visible={true} />
+      </Mapbox.MapView>
     </ThemedView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create(
+{
   container: {
     flex: 1,
     alignItems: 'center',
