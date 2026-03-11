1e7ce9c092bf38c1ed3f67cd9cd80eb49ee6604d
ArandomF <westoncarmack@gmail.com>
2026-03-11 15:52:28 -0400

    Start of the sessions module

--- backend/src/modules/sessions/service.ts
+++ backend/src/modules/sessions/service.ts
@@ -11,12 +11,21 @@ export abstract class UserService{
     }
 
     // Remove a user session from the sessions table
-    static async deleteSession(){
-
-    }
-
-    // Retrieve the users location for the current location table
-    static async getLocation(){
+    static async deleteSession( {sessionID}: Session.deleteSession){
         
+        // Check if the session exists in the database
+        const [session] = await db`
+        SELECT sessionID FROM sessions
+        WHERE sessionID = ${sessionID}
+        LIMIT 1`;
+    
+        if (!session) throw status (404, "Session not found");
+
+        // Delete the session from the database
+        await db`
+        DELETE FROM sessions
+        WHERE sessionID = ${sessionID}
+        `
     }
+    
 }
